bbfb4ddf621a75d3b443d63bdb546e843a14419a
Pelesa <53566770+Pelesa@users.noreply.github.com>
2020-09-04 15:14:12 +0200

    CalidadImagen+PorcentPixels

--- organiza.py
+++ organiza.py
@@ -13,6 +13,7 @@ with open('configuration.json') as f:
   config = json.load(f)
   config = SimpleNamespace(**config)
 
+
 def guardar(fecha,matricula,imagen):
     fecha = "/".join(fecha)
     au="au.pdf"
@@ -39,13 +40,13 @@ PDF_file = "example.pdf"
 ''' 
 Part #1 : Converting PDF to images 
 '''
-if os.name == 'nt': pytesseract.pytesseract.tesseract_cmd = config["tesseract"]
+if os.name == 'nt': pytesseract.pytesseract.tesseract_cmd = config.tesseract
 # Store all the pages of the PDF in a variable 
-pages = convert_from_path(PDF_file, 500) 
+pages = convert_from_path(PDF_file, 600) 
 
 # Counter to store images of each page of PDF to image 
 image_counter = 1
-  
+
 
 # Iterate through all the pages stored above 
 for page in pages: 
@@ -53,8 +54,8 @@ for page in pages:
     imageMatricula = "page_"+str(image_counter)+"matricula"+".jpg"
     imageFecha = "page_"+str(image_counter)+"fecha"+".jpg"
     width, height = page.size
-    matricula = page.crop((0, 2700, 2800, 3000)) 
-    fecha = page.crop((width-600,0,width,400))  
+    matricula = page.crop((0, height*0.461933, width*0.6773, height*0.513259)) 
+    fecha = page.crop((width*0.854862,0,width,height*0.06843456))
 
     # Increment the counter to update filename 
     image_counter = image_counter + 1
